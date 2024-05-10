4172f153498f09601d078ab07eff4948fab4ffe9
Will <williammartinh@gmail.com>
2024-05-10 16:25:31 +0100

    Revert "push all to repo"
    This reverts commit d3f93c423dc4aca46bab0c707261c0dcea7ae4f9.

--- app/healthCheck/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from healthstatus.views import health_check  # Adjust app name as necessary
-
-urlpatterns = [
-    path('health/', health_check, name='health_check'),
-    path('admin/', admin.site.urls),
-]
--- app/healthstatus/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-def health_check(request):
-    return JsonResponse({"message": "Health check: status ok"}, status=200)
