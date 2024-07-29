387cd4e7a3c8df97b219034954e488e0ece6974c
Will <williammartinh@gmail.com>
2024-07-29 14:02:00 +0100

    updated base_Dir

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -8,14 +8,15 @@ import pymysql
 # Install pymysql as MySQLdb
 pymysql.install_as_MySQLdb()
 
-# Define BASE_DIR to point to the 'app' directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Define BASE_DIR to point to the root directory of the project
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.join(BASE_DIR, 'app')
 
 # Initialize environment variables
 env = environ.Env()
 
 # Reading .env file (ensure this is after BASE_DIR definition)
-env_file_path = os.path.join(BASE_DIR, 'app', 'secure_keys.env')
+env_file_path = os.path.join(APP_DIR, 'secure_keys.env')
 print("Reading .env file from:", env_file_path)
 
 # Ensure the .env file path is correct by checking its existence
