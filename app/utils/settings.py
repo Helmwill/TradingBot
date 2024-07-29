6c1963951b911e6fcb1d848120308410344d8351
Will <williammartinh@gmail.com>
2024-07-29 14:21:28 +0100

    removing 'app' from 'env' filpath

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env()
 
 # Reading .env file (ensure this is after BASE_DIR definition)
-env_file_path = os.path.join(BASE_DIR, 'app', 'secure_keys.env')
+env_file_path = os.path.join(BASE_DIR, 'secure_keys.env')
 print("Reading .env file from:", env_file_path)
 
 # Ensure the .env file path is correct by checking its existence
