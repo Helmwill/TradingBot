32c1314c3e65e9f150472c154f9d7a205b58cb96
Will <williammartinh@gmail.com>
2024-07-29 14:31:05 +0100

    changed file path... again

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -8,21 +8,22 @@ import pymysql
 # Install pymysql as MySQLdb
 pymysql.install_as_MySQLdb()
 
-# Define BASE_DIR to point to the root directory of the project
+# Define BASE_DIR to point to the root directory of the project (practice_route)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Initialize environment variables
 env = environ.Env()
 
 # Reading .env file (ensure this is after BASE_DIR definition)
-env_file_path = os.path.join(BASE_DIR, 'secure_keys.env')
-print("Reading .env file from:", env_file_path)
+env_file_path = os.path.join(BASE_DIR, 'app', 'secure_keys.env')
+print(f"Reading .env file from: {env_file_path}")
 
 # Ensure the .env file path is correct by checking its existence
 if not os.path.exists(env_file_path):
     raise FileNotFoundError(f"Expected .env file at {env_file_path}")
 
 environ.Env.read_env(env_file_path)
+
 # Read environment variables
 try:
     SECRET_KEY = env('DJANGO_SECRET_KEY')
